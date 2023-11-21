1396c86c7192c665949db69fb497c29af3714104
zowber <andy@zowber.com>
2023-11-21 11:18:28 +0000

    export as csv/json

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -38,6 +38,7 @@ func NewRouter() http.Handler {
 	mux.HandleFunc("/scripts/links.js", staticHandler)
 
 	mux.HandleFunc("/import", importHandler)
+	mux.HandleFunc("/export", exportHandler)
 
 	return mux
 }
@@ -76,8 +77,8 @@ var linksHandler = func(w http.ResponseWriter, r *http.Request) {
 				log.Print(err.Error())
 			}
 
-			tmpl := template.Must(template.New("links.html").Funcs(funcMap).ParseFiles("./templates/links.html", "./templates/header.html", "./templates/links-list.html", "./templates/link.html", "./templates/footer.html"))
-			tmpl.Execute(w, links)
+			tmpl := template.Must(template.New("links.html").Funcs(funcMap).ParseFiles("./templates/header.html", "./templates/links.html", "./templates/links-list.html", "./templates/link.html", "./templates/footer.html"))
+			tmpl.ExecuteTemplate(w, "links.html", links)
 		default:
 			errorHandler(w, r, http.StatusMethodNotAllowed, err)
 		}
@@ -420,8 +421,6 @@ var importHandler = func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.New("import.html").ParseFiles("./templates/header.html", "./templates/import.html", "./templates/footer.html"))
 		tmpl.ExecuteTemplate(w, "import", nil)
 	case "POST":
-		log.Println("post to importHandler")
-
 		file, _, err := r.FormFile("file")
 		if err != nil {
 			log.Println("Err reading file", err)
@@ -454,8 +453,6 @@ var importHandler = func(w http.ResponseWriter, r *http.Request) {
 			links = append(links, &link)
 		}
 
-		log.Println("links:", links)
-
 		action := r.MultipartForm.Value["action"][0]
 
 		if action == "preview" {
@@ -475,6 +472,49 @@ var importHandler = func(w http.ResponseWriter, r *http.Request) {
 
 			tmpl := template.Must(template.New("import-result").Parse("<p>Imported {{ . }} links.</p>"))
 			tmpl.Execute(w, len(links))
+		}
+
+	default:
+		errorHandler(w, r, http.StatusMethodNotAllowed, err)
+		return
+	}
+}
+
+var exportHandler = func(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		tmpl := template.Must(template.New("export.html").ParseFiles("./templates/header.html", "./templates/export.html", "./templates/footer.html"))
+		tmpl.ExecuteTemplate(w, "export", nil)
+	case "POST":
+		links, err := db.All()
+		if err != nil {
+			log.Println("Err getting all links", err)
+			errorHandler(w, r, http.StatusInternalServerError, err)
+		}
+
+		action := r.PostFormValue("action")
+		if action == "csv" {
+            w.Header().Set("Content-Disposition", "attachment; filename=export.csv")
+	        w.Header().Set("Content-Type", "text/csv")
+			writer := csv.NewWriter(w)
+
+			for _, link := range links {
+                var labels []string
+                for _, label := range link.Labels {
+                    labels = append(labels, label.Name)
+                }
+                record := []string{strconv.Itoa(*link.Id), link.Name, link.Url, strconv.Itoa(link.CreatedAt)}
+                record = append(record, labels...)
+				writer.Write(record)
+			}
+            writer.Flush()
+		}
+
+		if action == "json" {
+            w.Header().Set("Content-Disposition", "attachment; filename=export.json")
+	        w.Header().Set("Content-Type", "application/json")
+            writer := json.NewEncoder(w)
+            writer.Encode(links)
 		}
 
 	default:
