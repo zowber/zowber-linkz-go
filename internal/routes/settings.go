23749e86624c35ab8087dd47cea3aacf84d78c13
zowber <andy@zowber.com>
2023-11-30 18:48:40 +0000

    wip: update user/settings

--- internal/routes/settings.go
+++ internal/routes/settings.go
@@ -8,18 +8,37 @@ import (
 )
 
 func settingsHandler() http.HandlerFunc {
-    return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 
-        type PageProps struct {
-            Settings linkzapp.Settings
+		type PageProps struct {
+			Settings linkzapp.Settings
+		}
+
+		// get settings from DB
+		settings, _ := db.GetSettings()
+
+		props := PageProps{Settings: *settings}
+
+		if r.Method == "GET" {
+			tmpl := template.Must(template.New("settings").Funcs(funcMap).ParseFiles("./templates/settings.tmpl.html", "./templates/head.html", "./templates/header.html", "./templates/footer.html"))
+			tmpl.ExecuteTemplate(w, "settings", props)
+		}
+
+        if r.Method == "POST" {
+
+           //var (
+           //    name = r.PostFormValue("name")
+           //    colourScheme = r.PostFormValue("colour-scheme") 
+           //)
+           //
+           //newUser := linkzapp.User{
+           //    Name: name,
+           //}
+
+           //newSettings := linkzapp.Settings{
+           //    ColourScheme: colourScheme,
+           //}
+
         }
-
-        // get settings from DB
-        settings, _ := db.GetSettings()
-
-        props := PageProps{ Settings: *settings }
-
-        tmpl := template.Must(template.New("settings").Funcs(funcMap).ParseFiles("./templates/settings.tmpl.html", "./templates/head.html", "./templates/header.html", "./templates/footer.html"))
-        tmpl.ExecuteTemplate(w, "settings", props )
-    }
+	}
 }
