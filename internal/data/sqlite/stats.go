c64003f3a0c20e43fac01661f3f521315db3035b
zowber <andy@zowber.com>
2023-12-27 22:06:48 +0000

    wip: top labels

--- internal/data/sqlite/stats.go
+++ internal/data/sqlite/stats.go
@@ -2,6 +2,7 @@ package sqlite
 
 import (
 	"log"
+	"sort"
 	"time"
 
 	"github.com/zowber/zowber-linkz-go/pkg/linkzapp"
@@ -16,19 +17,40 @@ func (d *SQLiteClient) GetPopularLabelsByYearAndMonth() (linkzapp.PopularLabelsB
 		log.Println("Err", err)
 	}
 
-    latestUnixTime := links[0].CreatedAt
-    oldestUnixTime := links[len(links)-1].CreatedAt
+	labelCounts := make(map[string]map[string]int)
 
-    latestLinkTime := time.Unix(int64(latestUnixTime), 0)
-    oldestLinkTime := time.Unix(int64(oldestUnixTime), 0)
+	for _, link := range links {
+		month := time.Unix(int64(link.CreatedAt), 0).Format("2006-01")
+		if labelCounts[month] == nil {
+			labelCounts[month] = make(map[string]int)
+		}
+		for _, label := range link.Labels {
+			labelCounts[month][label.Name]++
+		}
+	}
 
-    latestYear := latestLinkTime.Year()
-    oldestYear := oldestLinkTime.Year()
+	// Extract top 3 labels for each month
+	topLabels := make(map[string][]string)
+	for month, counts := range labelCounts {
+		labels := make([]string, 0, len(counts))
+		for label := range counts {
+			if label != "" {
+				labels = append(labels, label)
+			}
+		}
 
-    currYear := oldestYear
-    for i := oldestYear; i <= latestYear ; i++ {
-        currYear++
-    }
+		sort.Slice(labels, func(i, j int) bool {
+			return counts[labels[i]] > counts[labels[j]]
+		})
+
+		if len(labels) > 3 {
+			labels = labels[:2]
+		}
+
+		topLabels[month] = labels
+	}
+
+	log.Println(topLabels)
 
 	return res, nil
 }
